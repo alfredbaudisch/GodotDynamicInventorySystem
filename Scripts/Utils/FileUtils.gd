2ebf5131800e75ed3233894ba0572fca46361b68
Alfred Reinold Baudisch <alfred@alfredbaudisch.com>
2024-02-13 20:51:37 +0000

    When dynamically getting file names from a path, accept multiple extensions, due to exported builds converting resources to binary files, then changing *.tres resources to *.remap. Temporary fix to solve and close #5.

--- Scripts/Utils/FileUtils.gd
+++ Scripts/Utils/FileUtils.gd
@@ -21,7 +21,7 @@ static func get_dir_names_from_path(path) -> Array:
 	return contents
 
 
-static func get_file_names_from_path(path, extension := "all") -> Array:
+static func get_file_names_from_path(path, extensions : Array = []) -> Array:
 	var contents = []	
 	var dir = DirAccess.open(path)
 	
@@ -29,21 +29,26 @@ static func get_file_names_from_path(path, extension := "all") -> Array:
 		dir.list_dir_begin() # TODOConverter3To4 fill missing arguments https://github.com/godotengine/godot/pull/40547
 		
 		var file_name = dir.get_next()		
-		while file_name != "":			
-			if not dir.current_is_dir() and (extension == "all" or extension == file_name.get_extension()):
+		while file_name != "":
+			if not dir.current_is_dir() and (extensions.is_empty() or extensions.has(file_name.get_extension())):
 				contents.append(file_name)
 				
 			file_name = dir.get_next()
 	else:
 		printerr("An error occurred when trying to access the path.")
-		
+
 	return contents
 
 
 static func load_resources_from_path(path) -> Array:
 	var resources = []
 	
-	for filename in get_file_names_from_path(path, "tres"):
+	for filename in get_file_names_from_path(path, ["tres", "remap"]):
+		# TODO: temp fix due to https://github.com/godotengine/godot/issues/66014
+		# Another solution is to change editor/export/convert_text_resources_to_binary
+		if filename.ends_with(".remap"):
+			filename = filename.trim_suffix(".remap")
+			
 		resources.append(load(path + "/" + filename))
 		
 	return resources	
